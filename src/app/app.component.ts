51d8e5f39d85847fa63f7525c6cac80259178f5f
Syed-Arieb <syedarieb56@gmail.com>
2024-08-31 19:36:45 +0500

    Implement title bar, actions menu, and quick actions bar.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { freeSet } from '@coreui/icons';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,5 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'SpreadNext';
+  icons = freeSet ;
 }
